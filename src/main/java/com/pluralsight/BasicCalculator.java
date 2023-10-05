ec3f207f8805e34befe907e5e79faabd854ce142
Randy23123 <rlopez@appdev.yearup.org>
2023-10-05 13:10:13 -0400

    choose possible calc

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
+    public static void main(String[] args)
+    {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        Float numb1 = scanner.nextFloat();
+
+        System.out.print("Enter a number: ");
+        Float numb2 = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.print("Choose a Possible Calculation \n (A)dd \n (S)ubtract \n (M)ultiply \n (D)ivide");
+        String name3 = scanner.nextLine();
+
+
+
+
+    }
 }
